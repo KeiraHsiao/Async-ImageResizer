4f407c0209088f79b4716508555484864ba90337
Keira <keirahsiao@gmail.com>
2019-09-20 01:22:47 +0800

    非同步練習_圖檔壓縮處理用非同步方式_v1
    非同步練習_圖檔壓縮處理用非同步方式_v1

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+
+            ImageProcess imageProcess = new ImageProcess();
+
+            imageProcess.Clean(destinationPath);
+
+            #region 原始版本
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            sw1.Stop();
+            #endregion
+
+            imageProcess.Clean(destinationPath);
+
+            #region 非同步版本
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw2.Stop();
+            #endregion
+
+            Console.WriteLine($"原始版本_花費時間: {sw1.ElapsedMilliseconds} ms");
+            Console.WriteLine($"非同步版本_花費時間: {sw2.ElapsedMilliseconds} ms");
+            Console.WriteLine(string.Format("效能提升百分比:{0} %" , ((double)(sw1.ElapsedMilliseconds - sw2.ElapsedMilliseconds) / (double)sw1.ElapsedMilliseconds)).ToString());
+        }
+    }
+}
